4e6fe9b3a5ff4b63a46b76ae40a60d9b3ae4f797
flippedouttt <bryce@flippedout.net>
2021-10-21 15:34:43 +0800

    Styling and images included

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,8 @@
 function Product ({product: {title, price, description, category, image}}) {
     return (
-      <div>
-        <div style={{backgroundImage: `url(${image})`}}></div>
-        <div>
+      <div className="flex bg-white w-full mb-5">
+        <div style={{backgroundImage: `url(${image})`}} className="w-5/12"></div>
+        <div className="w-7/12 p-12">
           <h1>{title}</h1>
           <h3>{category}</h3>
           <p>{price}$</p>
